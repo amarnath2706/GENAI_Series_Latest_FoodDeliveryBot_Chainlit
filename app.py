0e28dda9ae305ae1cec6c2656cfb50cc18bb2bc3
amarnath2706 <amarnath.arjsd@gmail.com>
2024-12-13 18:22:42 +0530

    commit

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from src.llm import ask_order, messages
 @cl.on_message
 async def main(message: cl.Message):
     # Your custom logic goes here...
-    
-
+    #our logic should mention here
     # Send a response back to the user
     await cl.Message(
         content=f"Received : {message.content}",
